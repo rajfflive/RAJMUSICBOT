200988e3cfcc4afe1a70b5eb5c6a7a50c75f8c9c
OG-RAJ <krishur32@gmail.com>
2026-08-10 20:07:33 +0530

    Add files via upload

---
+++ RAJBOTS_MUSICS/utils/decorators/__init__.py
@@ -0,0 +1,17 @@
+# 🔹 Customized & Branded by: Rajbots
+# 🔗 Project: https://github.com/rajfflive/RAJMUSICBOT/new/main
+# -----------------------------------------------
+# 🔸 SIMPLE MUSIC Project
+# 🔹 Developed & Maintained by: Rajbots (https://github.com/rajfflive/RAJMUSICBOT/new/main)
+# 📅 Copyright © 2026 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by Rajbots
+# -----------------------------------------------
+from .admins import *
+from .language import *
