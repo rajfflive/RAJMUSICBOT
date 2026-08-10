0998cf2430d95737761e2e374c4f1dd45f836956
OG-RAJ <krishur32@gmail.com>
2026-08-10 20:10:45 +0530

    Add files via upload

---
+++ RAJBOTS_MUSICS/utils/stream/autoclear.py
@@ -0,0 +1,32 @@
+# 🔹 Customized & Branded by: Rajbots
+# 🔗 Project: https://github.com/rajfflive/RAJMUSICBOT/new/main
+# -----------------------------------------------
+# 🔸 SIMPLE MUSIC Project
+# 🔹 Developed & Maintained by: Rajbots (https://github.com/rajfflive/RAJMUSICBOT/new/main)
+# 📅 Copyright © 2026 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by Rajbots
+# -----------------------------------------------
+import os
+from config import autoclean
+
+
+async def auto_clean(popped):
+    try:
+        rem = popped["file"]
+        autoclean.remove(rem)
+        count = autoclean.count(rem)
+        if count == 0:
+            if "vid_" not in rem or "live_" not in rem or "index_" not in rem:
+                try:
+                    os.remove(rem)
+                except:
+                    pass
+    except:
+        pass
